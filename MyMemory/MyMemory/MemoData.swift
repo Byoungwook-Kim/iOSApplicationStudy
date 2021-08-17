2940e6a7031837fc6e3a338c1eed0e1f4a31b06a
Byoungwook-Kim <Crucrat@gmail.com>
2021-08-17 23:51:12 +0900

    MemoApp FormVC

---
+++ MyMemory/MyMemory/MemoData.swift
@@ -0,0 +1,16 @@
+//
+//  MemoData.swift
+//  MyMemory
+//
+//  Created by Byoung_wook on 2021/08/17.
+//
+
+import UIKit
+
+class MemoData {
+    var memoIdx : Int?
+    var title : String?
+    var contents : String?
+    var image : UIImage?
+    var regdate : Data?
+}
---
+++ MyMemory/MyMemory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by Byoung_wook on 2021/08/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
